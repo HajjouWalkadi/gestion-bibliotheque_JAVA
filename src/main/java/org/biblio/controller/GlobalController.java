cd56dd8799f04310792155adabcea235569631f2
hajjou walkadi <walkadihajjou153@gmail.com>
2023-09-08 09:26:05 +0100

    l'ajout et l'affichage des collections

--- src/main/java/org/biblio/controller/GlobalController.java
+++ src/main/java/org/biblio/controller/GlobalController.java
@@ -1,17 +1,96 @@
 package org.biblio.controller;
 
+import org.biblio.model.Collection;
+import org.biblio.model.Livre;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static org.biblio.model.Collection.ajouterCollection;
+import static org.biblio.model.Collection.getCollections;
+
 public class GlobalController {
+    static Scanner sc = new Scanner(System.in);
     public static void displayBooks(){
-        System.out.println("display books executed");
+        List<Collection> collections = Collection.getCollections(null);
+        for (Collection collection:collections){
+            System.out.println("Id   :" +collection.getId());
+            System.out.println("Title   :" +collection.getTitre());
+
+            System.out.println("---------------------------------");
+        }
 
     }
     public static void addBook(){
 
     }
     public static void deleteBook(){
+        System.out.print("ID du livre à supprimer : ");
+        int livreId = sc.nextInt();
+        sc.nextLine(); // Pour consommer la nouvelle ligne après la saisie de l'ID
 
+        // Appelez la méthode pour supprimer le livre
+        Livre.supprimerLivre(livreId);
+
+        System.out.println("Le livre a été supprimé avec succès !");
     }
     public static void updateBook(){
-
+        System.out.print("numero du livre à modifié : ");
+        String numeroLivre = sc.nextLine();
+        Livre livre = Livre.checkExisting(numeroLivre);
+        if (livre.getId() == 0){
+            System.out.println("there is no books ith this number");
+            updateBook();
+        }else {
+           livre =  getUpdatedBookInfo(livre);
+           livre.update();
+        }
     }
+
+    public static Livre getUpdatedBookInfo(Livre livre){
+        System.out.println("new book number");
+        livre.setNumeroLivre(sc.nextLine());
+        System.out.println("new book status");
+        livre.setStatus(sc.nextLine());
+        System.out.println("do you want to update the collection:");
+        System.out.println("1-No i do not");
+        System.out.println("2-Yes, To an existing collection");
+        System.out.println("3-Yes, To a collection that doesn't exist");
+
+        int choice = 0;
+        do {
+            choice = sc.nextInt();
+        }while (choice !=1 && choice !=2 && choice !=3);
+        if (choice == 1){
+            return livre;
+        }else{
+            if (choice == 2){
+                displayBooks();
+                System.out.println("choose the id of the collection");
+                Collection collection = new Collection();
+                collection.setId(sc.nextInt());
+                livre.setCollection(collection);
+            }else{
+                sc.nextLine();
+                Collection collection = new Collection();
+                System.out.print("Titre de la collection : ");
+                String titre = sc.nextLine();
+                System.out.print("Auteur de la collection : ");
+                String auteur = sc.nextLine();
+                System.out.print("ISBN de la collection : ");
+                String isbn = sc.nextLine();
+                System.out.print("Quantité de la collection : ");
+                int quantity = sc.nextInt();
+                sc.nextLine();
+                Collection nouvelleCollection = new Collection(0, titre, auteur, isbn, quantity);
+
+                ajouterCollection(nouvelleCollection);
+                collection =  Collection.getCollections(nouvelleCollection.getTitre()).get(0);
+                livre.setCollection(collection);
+                System.out.println("La collection a été ajoutée avec succès !");
+            }
+        }
+        return livre;
+    }
+
 }
--- src/main/java/org/biblio/model/Collection.java
+++ src/main/java/org/biblio/model/Collection.java
@@ -100,10 +100,12 @@ public class Collection {
 */
 
 
+/*
 package org.biblio.model;
 
 import org.biblio.database.Db;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -150,5 +152,154 @@ public class Collection {
     public int getId() {
         return id;
     }
+
+    public static void ajouterCollection(Collection collection) {
+        Connection connection = Db.connect();
+        if (connection == null) {
+            System.err.println("La connexion à la base de données a échoué.");
+            return;
+        }
+
+        String sql = "INSERT INTO collection (titre, auteur, ISBN, quantity) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, collection.getTitre());
+            preparedStatement.setString(2, collection.getAuteur());
+            preparedStatement.setString(3, collection.getISBN());
+            preparedStatement.setInt(4, collection.getQuantity());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Collection ajoutée avec succès !");
+            } else {
+                System.err.println("Échec de l'ajout de la collection.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
+*/
+
+package org.biblio.model;
+
+import org.biblio.database.Db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection {
+    private Integer id;
+    private String titre;
+    private String auteur;
+    private String ISBN;
+    private Integer quantity;
+
+    public Collection(Integer id, String titre, String auteur, String ISBN, Integer quantity) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.ISBN = ISBN;
+        this.quantity = quantity;
+    }
+    public Collection(){}
+
+    // Getter methods for attributes
+    public Integer getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public static List<Collection> getCollections(String titre) {
+        List<Collection> collections = new ArrayList<Collection>();
+        String sqlQuery = "SELECT * FROM collection";
+        if (titre != null){
+            sqlQuery += " WHERE titre = ?";
+        }
+        try {
+            PreparedStatement preparedStatement = Db.connect().prepareStatement(sqlQuery);
+            if (titre != null){
+                preparedStatement.setString(1,titre);
+            }
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Collection collection = new Collection(
+                        resultSet.getInt("id"),
+                        resultSet.getString("titre"),
+                        resultSet.getString("auteur"),
+                        resultSet.getString("ISBN"),
+                        resultSet.getInt("quantity")
+                );
+                collections.add(collection);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return collections;
+    }
+
+    public static void ajouterCollection(Collection collection) {
+        Connection connection = Db.connect();
+        if (connection == null) {
+            System.err.println("La connexion à la base de données a échoué.");
+            return;
+        }
+
+        String sql = "INSERT INTO collection (titre, auteur, ISBN, quantity) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, collection.getTitre());
+            preparedStatement.setString(2, collection.getAuteur());
+            preparedStatement.setString(3, collection.getISBN());
+            preparedStatement.setInt(4, collection.getQuantity());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Collection ajoutée avec succès !");
+            } else {
+                System.err.println("Échec de l'ajout de la collection.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 
